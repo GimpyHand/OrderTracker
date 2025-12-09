0455183153efdd036a6670e719e0043815f8b466
Alex Jessup <alexjessup@proton.me>
2025-12-09 06:56:25 -0600

    Used pseudo code to outline the process for the orders on the orders.html page in the scrpt.js file.

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+/*
+App Flow
+1. User opens order page and orders appear in top bar
+2. User actions each order and starts the process with a card being created in the main container. Each new order is prepended/appended to the list.
+3. Order moves to order picking stage
+4. Order moves to order packing stage
+5. Order moves to order shipping stage
+6. Order moves to order delivery stage
+7. User can clear completed orders from the order page
+8. Data about number of orders, types of content, pricing is all displayed in a simple dashboard in dashboard.html
+*/
+
