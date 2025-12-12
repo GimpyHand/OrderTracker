62fa3541885ebadb1e66760dd0143f3fb08874d8
Alex Jessup <alexjessup@proton.me>
2025-12-12 10:31:51 -0600

    Remove extra example order cards from orders page and completed pseudo-code for jaavscript logic

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,14 @@
 /*
 App Flow
-1. User opens order page and orders appear in top bar
-2. User actions each order and starts the process with a card being created in the main container. Each new order is prepended/appended to the list.
+1. User opens order page and clicks on a button to start receiving orders and orders appear in top bar
+2. Orders come in randomly with a specific shipper. A container that displays one of the shippers at a time indicates which orders to action.
+2. User actions each order (start button) and starts the process with a card being created in the main container. Each new order is prepended/appended to the list.
 3. Order moves to order picking stage
 4. Order moves to order packing stage
 5. Order moves to order shipping stage
 6. Order moves to order delivery stage
 7. User can clear completed orders from the order page
-8. Data about number of orders, types of content, pricing is all displayed in a simple dashboard in dashboard.html
+8. Set random timers for steps within maximmum times. Set ability (1 in 50 randomly) that fail delivery
+9. Data about number of orders, types of content, pricing is all displayed in a simple dashboard in dashboard.html
 */
 
